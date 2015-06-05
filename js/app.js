6f8408bda4365f3edcd2785155cd9987b9754ef3
HarrietM <hsemckinnon@gmail.com>
2015-06-05 14:08:59 +1200

    basic angular routes

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var webApp = angular.module('webApp', ['ngRoute'
+]);
+
+webApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/projects/c4', {
+        templateUrl: './projects/c4/index.html',
+      }).
+      otherwise({
+        redirectTo: '/',
+        templateUrl: './views/index.html',
+      });
+  }]);
+
+
+
---
+++ projects/c4/js/application.js
@@ -0,0 +1,6 @@
+$("#c4").ready(function(){
+
+  var board = new boardController("#c4");
+  board.run();
+
+});
